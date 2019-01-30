4ffb54d55a475267a8ea7abba88c305b27b5b432
Rafaelfferreira <rafaelfca.ferreira@gmail.com>
2019-01-30 01:04:58 -0200

    Program now accepts multiple delimiters

--- StringCalculatorKataProject/StringCalculatorKata/StringCalculatorKata/Class1.cs
+++ StringCalculatorKataProject/StringCalculatorKata/StringCalculatorKata/Class1.cs
@@ -12,24 +12,53 @@ namespace StringCalculatorKata
             if (!string.IsNullOrEmpty(numbers)) //test if the input is not empty.
             {
                 List<string> ListaDelimitador = new List<string>();
+                List<string> delimitadores = new List<string>() { "," };
 
                 if (numbers[0] == '/') //if there is a specific delimitador other than "," and "\n"
                 {
-                    int i = 2; //index of the first digit of the delmitador
-                    while (numbers[i] != '\n') //until it finds "\n"
+                    if (numbers[2] == '[') //multiple delimitadors
                     {
-                        ListaDelimitador.Add(System.Convert.ToString(numbers[i]));
-                        i++;
+                        int i = 2; //index of the first [
+                        while (numbers[i] != '\n') //until it finds "\n" (the end of the delimitador list)
+                        {
+                            ListaDelimitador = new List<string>();
+                            do
+                            {
+                                if(numbers[i] != '[')
+                                    ListaDelimitador.Add(System.Convert.ToString(numbers[i]));
+                                i++;
+                            } while (numbers[i] != ']'); //until it finds "]" (the end of the first delimitador)
+
+                                delimitadores.Add(String.Join("", ListaDelimitador));
+                                i++;
+                        }
+
+                        numbers = numbers.Substring(i + 1); //split the string into the substring without the delimitador
+
                     }
+                    else //only one custom delimitador
+                    {
+                        int i = 2; //index of the first digit of the delmitador
+                        while (numbers[i] != '\n') //until it finds "\n"
+                        {
+                            ListaDelimitador.Add(System.Convert.ToString(numbers[i]));
+                            i++;
+                        }
 
-                    numbers = numbers.Substring(i + 1); //split the string into the substring without the delimitador
+                        numbers = numbers.Substring(i + 1); //split the string into the substring without the delimitador
+
+                        delimitadores.Add(String.Join("", ListaDelimitador)); //joins the itens on ListaDelimitador into a single string
+                    }
                 }
-                else
-                    ListaDelimitador.Add(",");
 
-                string delimitador = String.Join("", ListaDelimitador); //joins the itens on ListaDelimitador into a single string
-                numbers = numbers.Replace("\n", delimitador); //substitutes all line breaks with commas
-                string[] digits = Regex.Split(numbers, delimitador);
+                string oldDel = "\n"; //var used to sort through all the delimitador on the string
+                for(int i=0; i < delimitadores.Count; i++)
+                {
+                    numbers = numbers.Replace(oldDel, delimitadores[i]); //substitutes all delimitadors with the last one available
+                    oldDel = delimitadores[i];
+                }
+                
+                string[] digits = Regex.Split(numbers, delimitadores[delimitadores.Count-1]);
                 
                 int sum = 0;
                 
